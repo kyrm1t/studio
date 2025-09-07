27920838fbeda47961e4d952896c823c8fb0afbb
K P <kparisagafonov1@gmail.com>
2025-09-07 19:26:46 +0000

    could you do all of those things?

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mtg.lifecounter',
+  appName: 'MTG Life Counter',
+  webDir: 'out',
+  bundledWebRuntime: false,
+};
+
+export default config;
